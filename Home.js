232f36bfea1929a582e7731673ba02c88abdc8da
AnselmusErik <anselmuserik11@gmail.com>
2024-02-24 23:57:26 +0700

    Update

--- Home.js
+++ Home.js
@@ -1,19 +1,19 @@
 // scripts.js
+
+// Smooth scrolling for navigation links
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', function (e) {
-        var targetElement = document.querySelector(this.getAttribute('href'));
+        const targetElement = document.querySelector(this.getAttribute('href'));
         if (targetElement) {
             e.preventDefault();
-            targetElement.scrollIntoView({
-                behavior: 'smooth'
-            });
+            targetElement.scrollIntoView({ behavior: 'smooth' });
         }
     });
 });
 
-
-// Coba galery
+// Event listener for DOMContentLoaded
 window.addEventListener('DOMContentLoaded', (event) => {
+    // Carousel gallery
     const items = document.querySelectorAll('.carousel-item');
     items.forEach(item => {
         item.addEventListener('click', () => {
@@ -21,26 +21,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
             item.classList.add('active');
         });
     });
-});
 
-// Coba Judul
-window.addEventListener('DOMContentLoaded', (event) => {
+    // Title color change on hover
     const title = document.getElementById('title');
+    title.addEventListener('mouseover', () => title.style.color = '#28a745');
+    title.addEventListener('mouseout', () => title.style.color = '#007bff');
+
+    // Subtitle color change on hover
     const subtitle = document.getElementById('subtitle');
-
-    title.addEventListener('mouseover', () => {
-        title.style.color = '#28a745';
-    });
-
-    title.addEventListener('mouseout', () => {
-        title.style.color = '#007bff';
-    });
-
-    subtitle.addEventListener('mouseover', () => {
-        subtitle.style.color = '#17a2b8';
-    });
-
-    subtitle.addEventListener('mouseout', () => {
-        subtitle.style.color = '#6c757d';
-    });
+    subtitle.addEventListener('mouseover', () => subtitle.style.color = '#17a2b8');
+    subtitle.addEventListener('mouseout', () => subtitle.style.color = '#6c757d');
 });
