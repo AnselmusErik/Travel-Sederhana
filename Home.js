93205bef0912150333e3855a2c84dc36d94073ad
AnselmusErik <anselmuserik11@gmail.com>
2024-02-24 22:59:53 +0700

    Update

--- Home.js
+++ Home.js
@@ -10,3 +10,37 @@ document.querySelectorAll('.nav-link').forEach(link => {
         }
     });
 });
+
+
+// Coba galery
+window.addEventListener('DOMContentLoaded', (event) => {
+    const items = document.querySelectorAll('.carousel-item');
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            items.forEach(i => i.classList.remove('active'));
+            item.classList.add('active');
+        });
+    });
+});
+
+// Coba Judul
+window.addEventListener('DOMContentLoaded', (event) => {
+    const title = document.getElementById('title');
+    const subtitle = document.getElementById('subtitle');
+
+    title.addEventListener('mouseover', () => {
+        title.style.color = '#28a745';
+    });
+
+    title.addEventListener('mouseout', () => {
+        title.style.color = '#007bff';
+    });
+
+    subtitle.addEventListener('mouseover', () => {
+        subtitle.style.color = '#17a2b8';
+    });
+
+    subtitle.addEventListener('mouseout', () => {
+        subtitle.style.color = '#6c757d';
+    });
+});
